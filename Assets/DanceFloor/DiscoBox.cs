f26bb1c05b373f675aefe5ad44fc6d80f3ec8665
Kunal Mansukhani <kunalmansukhani@Kunals-MacBook-Pro.local>
2024-09-28 14:26:33 -0400

    added 3 club walls to main dance floor

---
+++ Assets/DanceFloor/DiscoBox.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscoBox : MonoBehaviour
+{
+	public Vector3 moveTo;
+	public Vector3 currentPos;
+	public float pos_tolerance;
+	public float maxRange;
+	public float timeToWait;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start ()
+	{
+		currentPos = this.transform.position;
+		InvokeRepeating("MoveTheBox", 5.0f, timeToWait);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		this.transform.position = Vector3.MoveTowards (this.transform.position, moveTo, 0.1f);
+	}
+
+	void MoveTheBox()
+	{
+		//Get a new random position within our box
+		moveTo = new Vector3((Random.Range(-maxRange, maxRange)),0,Random.Range(-maxRange, maxRange));
+
+		if (moveTo.x < (this.transform.position.x + pos_tolerance) && (moveTo.z < (this.transform.position.z + pos_tolerance)))
+		{
+			//Advanced checking of tolerances, make a correction if beyond
+		}
+	}
+}
---
+++ Assets/DanceFloor/LookAtThis.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtThis : MonoBehaviour
+{
+	public Transform lookAtTarget;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.LookAt (lookAtTarget);
+	}
+}
